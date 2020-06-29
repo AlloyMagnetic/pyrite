3e606870c2423b281fe5a3ad3cba628effc1b2d0
Derek Adams <derek@alloymagnetic.com>
2020-06-29 11:49:54 -0400

    update to use a service and provide new rules

---
+++ src/PyriteRules.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\pyrite;
+
+/**
+ * Class PyriteRules.
+ */
+class PyriteRules {
+
+  /**
+   * Constructs a new PyriteRules object.
+   */
+  public function __construct() {
+
+  }
+
+  public static function nameMatch($values) {
+    $match_value = false;
+    foreach ($values as $value) {
+      if ($match_value) {
+        if ($value == $match_value) {
+          return [$value, $match_value];
+        }
+        else {
+          $match_value = $value;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public static function prohibitedWords($values) {
+    $prohibited = ['arse', 'ballsack', 'bastard', 'bastards', 'bitch', 'bitches', 'bitcoin', 'bitcoins', 'blockchain', 'blockchains', 'blowjob', 'blowjobs', 'blow job', 'blow jobs', 'boner', 'boners', 'boob', 'boobs', 'buttplug', 'clitoris', 'clit', 'cock', 'coon', 'cunt', 'dick', 'dildo', 'dyke', 'fellate', 'fellatio', 'fuck', 'Goddamn', 'God damn', 'hentai', 'jerk', 'jizz', 'labia', 'lesbian', 'muff', 'nigger', 'nigga', 'penis', 'piss', 'poop', 'porn', 'pube', 'pussy', 'queer', 'scrotum', 'shit', 'slut', 'smegma', 'spunk', 'tit', 'tits', 'tosser', 'turd', 'twat', 'vagina', 'wank', 'whore', 'online dating', 'internet dating', 'guest post', 'teens', 'teenagers', 'testosterone', 'payday loan', 'payday loans', 'personal loan', 'personal loans', 'seo metrics', 'search engine', 'search engines', 'anime', 'viagra', 'cialis', 'drugs', 'killer', 'unsubscribe', 'lead generation', 'SEO', 'mortgage', 'click here'];
+    foreach ($values as $value) {
+      foreach ($prohibited as $word) {
+        if (stripos($value, " $word") !== false) {
+          return $word;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public static function russianEmail($values) {
+    foreach ($values as $value) {
+      $value = trim($value);
+      $check_value = substr($value, -3);
+      if ($check_value == '.ru') {
+        return $value;
+      }
+    }
+    return false;
+  }
+  
+  public static function phoneZero($values) {
+    foreach ($values as $value) {
+      $value = trim($value);
+      $value = preg_replace('/[^0-9]+/', '', $value);
+      $check_value = substr($value, 0, 1);
+      if ($check_value == '0') {
+        return $value;
+      }
+    }
+    return false;
+  }
+  
+  public static function multipleUrls($values) {
+    foreach ($values as $value) {
+      $match = preg_match_all('@((https?://)?([-\\w]+\\.[-\\w\\.]+)+\\w(:\\d+)?(/([-\\w/_\\.]*(\\?\\S+)?)?)*)@', $value);
+      if ($match > 1) {
+        return $value;
+      }
+    }
+    return false;
+  }
+  
+  public static function bitly($values) {
+    foreach ($values as $value) {
+      if (strpos($value, 'bit.ly')) {
+        return $value;
+      }
+    }
+    return false;
+  }
+  
+  public static function localDomain($values) {
+    foreach ($values as $value) {
+      if (strpos($value, \Drupal::request()->getHost())) {
+        return $value;
+      }
+    }
+    return false;
+  }
+}
+
